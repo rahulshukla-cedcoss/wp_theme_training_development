0d5ce9d1ad08b390042ce12f774559b281ea918e
Rahul Shukla <rahulshukla@cedcoss.com>
2020-06-01 23:33:24 +0530

    custom header

--- wp-content/themes/mythemes/functions.php
+++ wp-content/themes/mythemes/functions.php
@@ -38,3 +38,19 @@ function wpb_custom_new_menu() {
 add_action( 'init', 'wpb_custom_new_menu' );
 
 add_theme_support( 'post-thumbnails' );
+
+/**
+ * Custom header
+ */
+function themename_custom_header_setup() {
+	$args = array(
+        'default-image'      => get_template_directory_uri() . 'img/default-image.jpg',
+    'default-text-color' => '000',
+        'width'              => 1000,
+        'height'             => 250,
+        'flex-width'         => true,
+        'flex-height'        => true,
+    )
+	add_theme_support( 'custom-header', $args );
+}
+add_action( 'after_setup_theme', 'themename_custom_header_setup' );
