9de270257034bd700643603a682bfb9f0ee28a8d
Rahul Shukla <rahulshukla@cedcoss.com>
2020-06-02 09:04:00 +0530

    custome header

--- wp-content/themes/mythemes/functions.php
+++ wp-content/themes/mythemes/functions.php
@@ -44,13 +44,13 @@ add_theme_support( 'post-thumbnails' );
  */
 function themename_custom_header_setup() {
 	$args = array(
-        'default-image'      => get_template_directory_uri() . 'img/default-image.jpg',
-    'default-text-color' => '000',
-        'width'              => 1000,
-        'height'             => 250,
-        'flex-width'         => true,
-        'flex-height'        => true,
-    )
+		'default-image'      => get_template_directory_uri() . 'img/default-image.jpg',
+		'default-text-color' => '000',
+		'width'              => 1000,
+		'height'             => 250,
+		'flex-width'         => true,
+		'flex-height'        => true,
+	);
 	add_theme_support( 'custom-header', $args );
 }
 add_action( 'after_setup_theme', 'themename_custom_header_setup' );
